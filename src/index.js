80cea987ea61a8dd8faa3f2540b154215ff7f2fd
Chiranjeev Vishnoi <somu209e@gmail.com>
2022-07-04 09:58:02 +0530

    learned about createStore

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from "redux";
+import {createStore}  from "redux";
 
 import App from './components/App';
 import './index.css';
 import movies from './reducers';
 
+//createStore expects an argument and we will pass reducer(movies) as our argument.
 const store=createStore(movies);
+//createstore will call our movies reducer which will tell the store its initial or new state and then store will merge the new state provided by reducers.
 console.log('store',store)
+console.log('STATE',store.getState())
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
