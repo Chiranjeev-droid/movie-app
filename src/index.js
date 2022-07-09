06e0876b7144fd66f9c3437301603d436d771e45
Chiranjeev Vishnoi <somu209e@gmail.com>
2022-07-09 09:35:06 +0530

    added logger middleware to console action types

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createStore}  from "redux";
+import {createStore,applyMiddleware}  from "redux";
 
 import App from './components/App';
 import './index.css';
 import rootReducer from './reducers';
 
+//function logger(obj,next,action)
+//logger(obj)(next)(action)
+const logger= function({dispatch,getState}){
+  return function(next){
+    return function(action){
+      //middleware code
+      console.log("ACTION_TYPE=",action.type);
+      next(action);
+    }
+  }
+}
+
+
 
 //createStore expects an argument and we will pass reducer(movies) as our argument.
-const store=createStore(rootReducer);
+const store=createStore(rootReducer,applyMiddleware(logger));
 //createstore will call our movies reducer which will tell the store its initial or new state and then store will merge the new state provided by reducers.
 console.log('store',store)
 console.log('STATE before sending action',store.getState())
